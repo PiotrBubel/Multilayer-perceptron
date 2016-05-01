05a8692835bed83bc52feb639fde1345efb7457b
Piotr Bubel <189633@edu.p.lodz.pl>
2016-05-01 22:18:07 +0200

    added classes with implementation of neurons and network

---
+++ src/main/java/com/mycompany/perceptron/ConnectedNeuralNetwork.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.perceptron;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Piotrek
+ */
+public class ConnectedNeuralNetwork {
+
+    //przedział z którego losowane są wagi początkowe neuronów
+    public static double MIN_FIRST_WAGE = -0.5d;
+    public static double MAX_FIRST_WAGE = 0.5d;
+    private List<ConnectedNeuron[]> layers;
+
+
+    public ConnectedNeuralNetwork(int inputNeurons, int outputNeurons, int hiddenNeurons, int hiddenLayers) {
+
+        //tworzenie warstw
+        layers = new ArrayList<>();
+        layers.add(0, new ConnectedNeuron[inputNeurons]);
+        for (int i = 1; i < hiddenLayers + 1; i++) {
+            layers.add(i, new ConnectedNeuron[hiddenNeurons]);
+        }
+        layers.add(hiddenLayers + 1, new ConnectedNeuron[outputNeurons]);
+
+        //wypełnianie pierwszej warstwy (input)
+        for (int i = 0; i < inputNeurons; i++) {
+            layers.get(0)[i] = new ConnectedNeuron(new double[]{1.0}, i);
+        }
+
+        //wypełnianie warstw ukrytych
+        for (int i = 0; i < hiddenLayers; i++) {
+            for (int j = 0; j < hiddenNeurons; j++) {
+                //neurony w każdej kolejnej warstwie mają tyle wejść ile jest neuronów w warstwie poprzedniej
+                layers.get(i + 1)[j] = new ConnectedNeuron(randWages(layers.get(i).length), j);
+            }
+        }
+
+        //wypełnianie ostatniej warstwy (output)
+        for (int i = 0; i < outputNeurons; i++) {
+            layers.get(hiddenLayers + 1)[i] = new ConnectedNeuron(randWages(layers.get(hiddenLayers).length), i);
+        }
+        connectNeurons();
+    }
+
+    public double[] lastHiddenLayerOutput(){
+        List<Double> outputsList = new ArrayList<>();
+
+        for(ConnectedNeuron n : this.layers.get(layers.size()-2)){
+            outputsList.add(n.getLastOutput());
+        }
+
+        return listToArray(outputsList);
+    }
+
+    private void connectNeurons() {
+        for (int i = 0; i < layers.size(); i++) {
+            for (int j = 0; j < layers.get(i).length; j++) {
+                if (i == 0) {
+                    layers.get(i)[j].setNextLayer(layers.get(i + 1));
+                } else if (i == layers.size() - 1) {
+                    layers.get(i)[j].setPreviousLayer(layers.get(i - 1));
+                } else {
+                    layers.get(i)[j].setNextLayer(layers.get(i + 1));
+                    layers.get(i)[j].setPreviousLayer(layers.get(i - 1));
+                }
+            }
+        }
+    }
+
+    private double[] randWages(int length) {
+        double[] wages = new double[length];
+        Random r = new Random();
+        for (int i = 0; i < length; i++) {
+            wages[i] = MIN_FIRST_WAGE + (MAX_FIRST_WAGE - MIN_FIRST_WAGE) * r.nextDouble();
+        }
+        return wages;
+    }
+
+    public double[] output(double[] x) {
+
+        List<Double> outputsList = new ArrayList<>();
+        //double[] outputsArray = new double[x.length];
+        for (int i = 0; i < x.length; i++) {
+            outputsList.add(i, layers.get(0)[i].outputWithoutSigmoid(x[i]));
+        }
+
+/*
+        List<Double> outputsList2 = new ArrayList<>();
+        for (int i = 0; i < layers.get(1).length; i++) {
+            outputsList2.add(layers.get(1)[i].output(listToArray(outputsList)));
+        }
+        List<Double> outputsList3 = new ArrayList<>();
+        for (int i = 0; i < layers.get(2).length; i++) {
+            outputsList3.add(layers.get(2)[i].output(listToArray(outputsList2)));
+        }
+        return listToArray(outputsList3);
+*/
+        List<Double> secondList = new ArrayList<>();
+        for (int l = 1; l < layers.size(); l++) {
+            for (int n = 0; n < layers.get(l).length; n++) {
+                if (l % 2 == 1) {
+                    secondList.add(n, layers.get(l)[n].output(listToArray(outputsList)));
+                } else {
+                    outputsList.add(n, layers.get(l)[n].output(listToArray(secondList)));
+                }
+            }
+            if (l % 2 == 1) {
+                outputsList.clear();
+            } else {
+                secondList.clear();
+            }
+        }
+        if (secondList.isEmpty()) {
+            return listToArray(outputsList);
+        } else {
+            return listToArray(secondList);
+        }
+    }
+
+    //wsteczna propagacja błędu
+    private void learn(double[] expected) {
+        //System.out.println("NAUKA--------------------------");
+        for (int i = layers.size() - 1; i > 0; i--) {//przechodzimy warstwa po warstwie od outputu w dół
+            for (int j = 0; j < layers.get(i).length; j++) {
+                if (i == layers.size() - 1) {
+                    layers.get(i)[j].calculateError(expected[j]);
+                } else {
+                    layers.get(i)[j].calculateError();
+                }
+            }
+        }
+        //najpierw wyliczamy błędy, potem zmieniamy wagi
+        for (int i = layers.size() - 1; i > 0; i--) {//przechodzimy warstwa po warstwie od outputu w dół
+            for (int j = 0; j < layers.get(i).length; j++) { //for (int j = layers.get(i).size() - 1; j >= 0; j--) {
+                //connectNeurons();
+                layers.get(i)[j].changeWages();
+            }
+        }
+    }
+
+    //wsteczna propagacja błędu
+    public void learn(double[] input, double[] expected) {
+        output(input);
+        learn(expected);
+    }
+
+    public void learn(List<Double> input, List<Double> expected) {
+        output(input);
+        learn(listToArray(expected));
+    }
+
+    public double[] output(List<Double> x) {
+        return output(listToArray(x));
+    }
+
+    private double[] listToArray(List<Double> x) {
+        double[] target = new double[x.size()];
+        for (int i = 0; i < target.length; i++) {
+            target[i] = x.get(i);
+        }
+        return target;
+    }
+}
---
+++ src/main/java/com/mycompany/perceptron/ConnectedNeuron.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.perceptron;
+
+import java.util.Random;
+
+/**
+ * @author Piotrek
+ */
+public class ConnectedNeuron {
+
+    public static double BETA = 1.0d;
+    public static double STEP = 0.9d;
+    public static double MOMENTUM = 0.6d;
+    public static boolean BIAS_ENABLED = false;
+
+    private final int indexInLayer;
+    private double[] w;
+    private double error;
+    private double lastOut;
+    private double lastSum;
+    private double[] lastW;
+
+    private double biasWage = 0d;
+    private double lastBiasWage = 0d;
+
+    private ConnectedNeuron[] nextLayer;
+    private ConnectedNeuron[] previousLayer;
+
+
+    public ConnectedNeuron(double[] wages, int index) {
+        this.w = wages.clone();
+        this.previousLayer = null;
+        this.nextLayer = null;
+        this.indexInLayer = index;
+        lastW = new double[w.length];
+        error = Double.NaN;
+        lastSum = Double.NaN;
+        lastOut = Double.NaN;
+        if (BIAS_ENABLED) {
+            Random r = new Random();
+            biasWage = lastBiasWage =
+                    ConnectedNeuralNetwork.MIN_FIRST_WAGE + (ConnectedNeuralNetwork.MAX_FIRST_WAGE - ConnectedNeuralNetwork.MIN_FIRST_WAGE) * r.nextDouble();
+        }
+    }
+
+    public double getError() {
+        return this.error;
+    }
+
+    public double getLastOutput() {
+        return this.lastOut;
+    }
+
+    public double getWage(int index) {
+        return this.w[index];
+    }
+
+    public void setNextLayer(ConnectedNeuron[] out) {
+        this.nextLayer = out;
+    }
+
+    public void setPreviousLayer(ConnectedNeuron[] in) {
+        this.previousLayer = in;
+    }
+
+    public double output(double[] x) {
+        lastSum = 0.0d;
+        for (int i = 0; i < x.length; i++) {
+            lastSum = lastSum + (x[i] * w[i]);
+        }
+        if (BIAS_ENABLED) {
+            lastSum = lastSum + biasWage * 1;
+        }
+        lastOut = sigmoidFunction(lastSum);
+        return lastOut;
+    }
+
+    public double outputWithoutSigmoid(double x) {
+        lastSum = 0.0d;
+        //for (int i = 0; i < x.length; i++) {
+        lastSum = (x * w[0]);
+        //}
+        if (BIAS_ENABLED) {
+            lastSum = lastSum + biasWage * 1;
+        }
+        lastOut = lastSum;
+        return lastOut;
+    }
+
+    //expectedOutput wykorzystywane jest tylko wtedy kiedy jest to warstwa ostatnia, w innych jest pomijane
+    public void calculateError() {
+        double errorSum = 0.0d;
+        for (int j = 0; j < nextLayer.length; j++) {
+            errorSum = errorSum + (nextLayer[j].getError() * nextLayer[j].getWage(indexInLayer));
+        }
+        error = sigmoidFunctionDerivative(lastSum) * errorSum;
+    }
+
+    public void calculateError(double expectedOutput) {
+        //wzór dla ostatniej warstwy
+        error = sigmoidFunctionDerivative(lastSum) * (expectedOutput - lastOut);
+    }
+
+    public void changeWages() {
+        double[] lastWClone = cloneArray(lastW);
+        lastW = cloneArray(w);  //sprawdzic czy clone dziala tak jak mysle
+        for (int i = 0; i < w.length; i++) {
+            w[i] = w[i] + STEP * error * previousLayer[i].getLastOutput() + MOMENTUM * (w[i] - lastWClone[i]);
+        }
+        if (BIAS_ENABLED) {
+            double lastBiasTemp = biasWage;
+            biasWage = biasWage + STEP * error * 1 + MOMENTUM * (biasWage - lastBiasWage);
+            lastBiasWage = lastBiasTemp;
+        }
+    }
+
+    private double[] cloneArray(double[] toClone) {
+        double[] clone = new double[toClone.length];
+        for (int i = 0; i < toClone.length; i++) {
+            clone[i] = toClone[i];
+        }
+        return clone;
+    }
+
+    //sigmoida
+    private double sigmoidFunction(double x) {
+        return 1d / (1d + Math.exp(-1d * BETA * x));
+    }
+
+    //pochodna sigmoidy
+    private double sigmoidFunctionDerivative(double x) {
+        double sig = sigmoidFunction(x);
+        return sig * (1d - sig); //wynika to z definicji pochodnej funkcji sigmoidalnej
+    }
+}
